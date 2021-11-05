f190d279e888de33db5681f58037432edb76ee6c
wolf <ahmedragabyasin2020@gmail.com>
2021-11-05 20:53:59 +0200

    start e-shop project

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ], function(){
+        Auth::routes();
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::group(['middleware' => ['auth']], function() {
+           Route::resource('users',App\Http\Controllers\UserController::class);
+           Route::resource('roles',App\Http\Controllers\RoleController::class);
+
+
+        });
+    });
